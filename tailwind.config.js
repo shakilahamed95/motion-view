aa1cded59defd592ad126165a07a03ebe314194c
Your Name <you@example.com>
2022-08-07 21:15:29 +0600

    project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#5260DA",
+          secondary: "#0FCEEB",
+          accent: "#3A4256",
+          neutral: "#004DDA",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
