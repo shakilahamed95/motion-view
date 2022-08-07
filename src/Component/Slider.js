52fcdba69f52f3154df93a08644ef1466db88069
Your Name <you@example.com>
2022-08-07 22:23:13 +0600

    carosel added

---
+++ src/Component/Slider.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import cover1 from '../Images/cover1.webp'
+import cover2 from '../Images/cover2.jpg'
+import cover3 from '../Images/cover3.jpg'
+import cover4 from '../Images/cover4.jpg'
+
+const Slider = () => {
+    return (
+        <div>
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full">
+                    <img src={cover1} style={{ height: "80vh", width: "100%" }} alt='' class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" class="btn btn-circle">❮</a>
+                        <a href="#slide2" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" class="carousel-item relative w-full">
+                    <img src={cover2} alt='' style={{ height: "80vh", width: "100%" }} class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" class="btn btn-circle">❮</a>
+                        <a href="#slide3" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" class="carousel-item relative w-full">
+                    <img src={cover3} alt='' style={{ height: "80vh", width: "100%" }} class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" class="btn btn-circle">❮</a>
+                        <a href="#slide4" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" class="carousel-item relative w-full">
+                    <img src={cover4} alt='' style={{ height: "80vh", width: "100%" }} class="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" class="btn btn-circle">❮</a>
+                        <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Slider from '../Component/Slider';
 
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to home</h1>
+            <Slider></Slider>
         </div>
     );
 };
